5734daaa809b548310a5e68ec615691b9eca9691
Ecnirped11 <abdulrokibadebisi@gmail.com>
2025-05-13 09:01:32 +0100

    Update chbot1.py and requirements

--- chbot1.py
+++ chbot1.py
@@ -54,25 +54,16 @@ async def aiversion(update: Update, context: ContextTypes.DEFAULT_TYPE):
         num_response = pass_num_prompt.provide_location()
         await update.message.reply_text(f'{num_response}')
 
-def run_bot():
-    asyncio.set_event_loop(asyncio.new_event_loop())
+def run_flask():
+    port = int(os.environ.get('PORT', 5000))
+    flask_app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+   
+    threading.Thread(target=run_flask).start()
     app = ApplicationBuilder().token(access_token).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler('help', help))
     app.add_handler(MessageHandler(filters.TEXT, aiversion))
     print('Bot is running...')
     app.run_polling()
-
-flask_app = Flask(__name__)
-
-@flask_app.route('/')
-def index():
-    return "Bot is running."
-
-if __name__ == "__main__":
-    
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-
-    port = int(os.environ.get('PORT', 5000))
-    flask_app.run(host='0.0.0.0', port=port)
